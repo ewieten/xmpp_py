9f153872ffad487cecb32e59c1d5ceb3ad353bdb
Ewout Wieten <mrewieten@gmail.com>
2013-06-23 16:15:16 +0200

    Generalized script
    Extended config & configurability

--- xmpp-pybot.py
+++ xmpp-pybot.py
@@ -8,8 +8,6 @@ import os
 import pyglet
 print('Imported succesfully.')
 
-group = 'group-738@muc.xmpp.evolvehq.com'
-
 config = ConfigParser.ConfigParser()
 config.read('bot.conf')
 user=config.get('logondata','user')
@@ -17,24 +15,28 @@ passw=config.get('logondata','password')
 server=config.get('logondata','server')
 msg=config.get('botdata','spammessage')
 target=config.get('botdata','spamtarget')
+target=target+"@"+server
+debug=config.get('botdata','debug')
 print('Config loaded succesfully.')
 
-group = group+"/"+user
-print(group)
-client=Client('xmpp.evolvehq.com')
+if debug=1:
+    client=Client('xmpp.evolvehq.com',debug=['always'])
+else:
+    client=Client('xmpp.evolvehq.com')
+
 client.connect()
+print('Connected')
 
 if client.auth(user,passw):
-    print('Connected!')
+    print('Authenticated.')
 else:
     print('Invalid credentials')
+    exit
 
 def messageHandler(conn,mess_node): pass
 
 client.RegisterHandler('message',messageHandler)
 print('Handlers Registered')
 
-client.send(Presence(to=epic))
-
 while True:
-    client.send(Message(group,msg))
+    client.send(Message(target,msg))
